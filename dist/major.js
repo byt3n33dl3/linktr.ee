96332a0f7434beab2da41bac86ef97f96be2bce0
Sulaiman <151133481+Sulaimannabdul@users.noreply.github.com>
2024-03-26 02:34:59 +0800

    Create major.js

---
+++ dist/major.js
@@ -0,0 +1,52 @@
+[
+    {
+        "name": "Programming Tutorials",
+        "url": "https://example.com/programming-tutorials",
+        "icon": "programming-tutorials-icon.png"
+    },
+    {
+        "name": "Tech Reviews",
+        "url": "https://example.com/tech-reviews",
+        "icon": "tech-reviews-icon.png"
+    },
+    {
+        "name": "Project Showcases",
+        "url": "https://example.com/project-showcases",
+        "icon": "project-showcases-icon.png"
+    },
+    {
+        "name": "Coding Challenges",
+        "url": "https://example.com/coding-challenges",
+        "icon": "coding-challenges-icon.png"
+    },
+    {
+        "name": "Developer Interviews",
+        "url": "https://example.com/developer-interviews",
+        "icon": "developer-interviews-icon.png"
+    },
+    {
+        "name": "Web Development Guides",
+        "url": "https://example.com/web-development-guides",
+        "icon": "web-development-guides-icon.png"
+    },
+    {
+        "name": "Software Engineering Courses",
+        "url": "https://example.com/software-engineering-courses",
+        "icon": "software-engineering-courses-icon.png"
+    },
+    {
+        "name": "UI/UX Design Insights",
+        "url": "https://example.com/ui-ux-design-insights",
+        "icon": "ui-ux-design-insights-icon.png"
+    },
+    {
+        "name": "Tech Conference Talks",
+        "url": "https://example.com/tech-conference-talks",
+        "icon": "tech-conference-talks-icon.png"
+    },
+    {
+        "name": "Open Source Projects",
+        "url": "https://example.com/open-source-projects",
+        "icon": "open-source-projects-icon.png"
+    }
+]
