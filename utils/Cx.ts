a384d12b90037a62b5d34be0297cb394ed023a36
Sulaiman <151133481+Sulaimannabdul@users.noreply.github.com>
2024-03-25 20:33:05 +0800

    Create Cx.ts

---
+++ utils/Cx.ts
@@ -0,0 +1,6 @@
+type Cx = (...a: Array<undefined | null | string | boolean>) => string;
+export const cx: Cx = (...args) =>
+  args
+    .flat()
+    .filter((x) => x !== null && x !== undefined && typeof x !== 'boolean')
+    .join(' ');
